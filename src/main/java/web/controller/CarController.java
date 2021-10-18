01447acb6d945c8b077305cd65389ecd10318c66
max <max.com.07@mail.ru>
2021-10-18 16:37:52 +0400

    Exercise 2.2.2 done.

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -4,11 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
 import web.service.CarService;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 @Controller
 public class CarController {
@@ -16,9 +20,15 @@ public class CarController {
     @Autowired
     private CarService carService;
 
+
     @GetMapping(value = "/cars")
-    public String getCars(ModelMap model){
-        model.addAttribute("cars", carService.getCars(5));
+    public String getCars(@RequestParam(required = false, defaultValue = "0") Integer count, ModelMap model){
+        if (count > 0 & count < 5) {
+            model.addAttribute("cars", carService.getCars(count));
+        } else {
+            model.addAttribute("cars", carService.getCars(5));
+        }
         return "cars";
     }
+
 }
